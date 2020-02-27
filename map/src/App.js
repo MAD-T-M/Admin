fbdc59f7655e2b9c32e9e06b7191524b26f60002
NagataKouya <nagata.twi@gmail.com>
2020-02-27 16:32:25 +0900

    fix marker

--- map/src/App.js
+++ map/src/App.js
@@ -2,30 +2,49 @@ import React from "react"
 import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
+const data = [
+  {
+    lat: 35.171996,
+    lng: 136.88463,
+  },
+  {
+    lat: 35.170680,
+    lng: 136.884844,
+  },{
+    lat: 35.171873,
+    lng: 136.882527,
+  }
+]
+
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAXb0KtfhHlwuFK2uHKMDx-ElE3GQahOaQ",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAXb0KtfhHlwuFK2uHKMDx-ElE3GQahOaQ&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `800px` }} />,
+    containerElement: <div style={{ height: `600px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
   withGoogleMap
 )((props) =>
   <GoogleMap
-    defaultZoom={16}
+    defaultZoom={17}
     defaultCenter={{ lat: 35.170915, lng: 136.881537 }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: 35.170915, lng: 136.881537 }} onClick={props.onMarkerClick} />}
+    {data.map((item) => {
+     return <Marker position={{ lat: item.lat, lng: item.lng }} onClick={props.onMarkerClick} />
+    })}
   </GoogleMap>
 )
 
 export default class MyFancyComponent extends React.PureComponent {
   state = {
-    isMarkerShown: true,
+    isMarkerShown: false,
   }
 
   componentDidMount() {
+    data.map((item) => {
+     return console.log('i', item)
+    })
     this.delayedShowMarker()
   }
 
@@ -42,11 +61,16 @@ export default class MyFancyComponent extends React.PureComponent {
 
   render() {
     return (
-      <MyMapComponent
-        isMarkerShown={this.state.isMarkerShown}
-        onMarkerClick={this.handleMarkerClick}
-      />
+      <div>
+        <h1>管理者画面</h1>
+        <div>
+          <h1 style={{fontSize: '20px'}}>駐輪違反マップ</h1>
+          <MyMapComponent
+            isMarkerShown={this.state.isMarkerShown}
+            onMarkerClick={this.handleMarkerClick}
+          />
+        </div>
+      </div>
     )
   }
 }
-
